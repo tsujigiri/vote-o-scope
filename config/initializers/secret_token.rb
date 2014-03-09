2783802f4d77ab8fb51f6f18c275de345a97f081
Helge Rausch <helge@rausch.io>
2014-03-09 08:40:10 +0100

    rails new vote-o-scope

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VoteOScope::Application.config.secret_key_base = '36abb2f0c5d7baeb602c1322973f2de4f6d3c8aefb819edbd89f97f03b83a3400859ef74f46874f5506d74741d9e98512eb76cfd1889979b2e5e429d175ca8db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VoteOScope::Application.config.session_store :cookie_store, key: '_vote-o-scope_session'
