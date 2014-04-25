d2988929d91e9f6ad65317debedb16145b24e3a6
Helge Rausch <helge@rausch.io>
2014-04-25 10:23:09 +0200

    Add build status

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,10 +47,6 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
   config.before(:each) do
     DatabaseCleaner.start
   end
