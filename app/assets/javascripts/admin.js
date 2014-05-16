b2f4c049ecbfcdcdb54cac5503bc7595c453215d
Helge Rausch <helge@rausch.io>
2014-05-16 08:53:46 +0200

    Hide reasoning textareas when others are opened

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,6 +2,7 @@
   $(function () {
     $('a.edit-reasoning').click(function (e) {
       e.preventDefault();
+      $('.reasoning-input').hide()
       $(e.target).closest('tr').next().show()
     });
   });
